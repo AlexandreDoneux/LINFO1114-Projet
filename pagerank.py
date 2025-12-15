aca992f64327b24bbdd6b004ffcdc898bb21bd72
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2025-12-15 18:25:06 +0100

    Adding linear equation method + Some changes

--- pagerank.py
+++ pagerank.py
@@ -2,6 +2,45 @@
 
 import numpy as np
 
+
+def prepare_data(A: np.ndarray, alpha: float, v: np.ndarray) -> np.ndarray:
+    """
+    Prépare les données pour le calcul du PageRank en construisant la matrice Google G.
+    Args:
+        A (np.ndarray): Matrice d'adjacence.
+        alpha (float): parametre de téléportation (compris entre 0 et 1).
+        v (np.ndarray): Vecteur de personnalisation.
+    Returns:
+        np.ndarray: Matrice Google G.
+    """
+    n = A.shape[0]
+
+    v = v.reshape((n, 1))
+    # Normalisation de v
+    if np.sum(v) != 0:
+        v = v / np.sum(v)
+    else:
+        v = np.ones((n, 1)) / n
+
+    P = np.array(A, dtype=float)
+
+    for i in range(n):
+        row_sum = np.sum(P[i, :])
+        if row_sum == 0:
+            # Gestion Noeuds sans issue
+            P[i, :] = 1.0 / n  # On distribue uniformément la probabilité ? On ne veut pas une ligne de 0 ? -> vérifier
+        else:
+
+            P[i, :] /= row_sum
+
+    # La formule est G = alpha * P + (1 - alpha) * e * v^T
+    e = np.ones((n, 1))
+
+    G = alpha * P + (1 - alpha) * np.dot(e, v.T)  # v.T ?
+    return G
+
+# --------------------------------------------------------------------------------------------------------------------
+
 def pageRankLinear(A: np.ndarray, alpha: float, v: np.ndarray) -> np.ndarray:
     """
     Calcule le PageRank via résolution d'équations linéaire
@@ -14,7 +53,30 @@ def pageRankLinear(A: np.ndarray, alpha: float, v: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: scores pagerank (dans le même ordre que les lignes de la matrice d’adjacence)
     """
-    pass
+    n = A.shape[0]
+    G = prepare_data(A, alpha, v)
+    I = np.eye(n)
+
+    # (I - alpha P^T) x = (1 - alpha) v
+    # Extract P from G (see slide
+    P = (G - (1 - alpha) * np.ones((n, 1)) @ v.reshape(1, n)) / alpha
+
+    # Normalisation de v
+    v = v.reshape((n, 1))
+    if np.sum(v) != 0:
+        v = v / np.sum(v)
+    else:
+        v = np.ones((n, 1)) / n
+
+    # Linear system + solve
+    M = np.eye(n) - alpha * P.T
+    b = (1 - alpha) * v
+    x = np.linalg.solve(M, b)
+
+    # Normalisation pour éviter les problèmes de calculs
+    x = x / np.sum(x)
+
+    return x.flatten()
 
 
 def pageRankPower(A: np.ndarray, alpha: float, v: np.ndarray) -> np.ndarray:
@@ -35,35 +97,13 @@ def pageRankPower(A: np.ndarray, alpha: float, v: np.ndarray) -> np.ndarray:
     max_iter = 10000
     
     
-    
-    v = v.reshape((n, 1))
-    # Normalisation de v 
-    if np.sum(v) != 0:
-        v = v / np.sum(v)
-    else:
-        v = np.ones((n, 1)) / n
+    G = prepare_data(A, alpha, v)
 
-    P = np.array(A, dtype=float)
-    
-    for i in range(n):
-        row_sum = np.sum(P[i, :])
-        if row_sum == 0:
-            # Gestion Noeuds sans issue
-            P[i, :] = 1.0 / n
-        else:
-            
-            P[i, :] /= row_sum
+    #print("\n Matrice d'adjacence A :\n", A)
+    #print("\n Matrice de probabilite de transition P :\n", P)
+    #print("\n Matrice Google G :\n", G)
 
-    # La formule est G = alpha * P + (1 - alpha) * e * v^T
-    e = np.ones((n, 1))
-    
-    G = alpha * P + (1 - alpha) * np.dot(e, v.T)
-
-    print("\n Matrice d'adjacence A :\n", A)
-    print("\n Matrice de probabilite de transition P :\n", P)
-    print("\n Matrice Google G :\n", G)
-
-    x = np.sum(A, axis=0).reshape((n, 1)).astype(float)
+    x = np.sum(A, axis=0).reshape((n, 1)).astype(float) # ?
     
     if np.sum(x) == 0:
         x = np.ones((n, 1)) / n
